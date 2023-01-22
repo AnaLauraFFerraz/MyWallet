b3ab48849e738c21283ae06d8dcc0881763646c9
AnaLauraFFerraz <analaurafferraz@gmail.com>
2023-01-22 17:45:23 -0300

    signup static page

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,10 +1,9 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import styled from "styled-components";
 //import axios from "axios";
 // import { UserContext } from "../context/UserContext"
 import { Input } from "../components/Input"
 import { Button } from "../components/Button"
+import { Container, Title, Form, StyledLink } from "../style/commonStyles"
 
 export default function SignIn() {
 
@@ -15,50 +14,11 @@ export default function SignIn() {
         <Container>
             <Title>MyWallet</Title>
             <Form>
-                <Input name="email" type="email" placeholder="E-mail" />
-                <Input name="password" type="password" placeholder="Senha" />
+                <Input name="email" type="email" placeholder="E-mail" required />
+                <Input name="password" type="password" placeholder="Senha" required />
                 <Button type="submit" >Entrar</Button>
             </Form>
             <StyledLink to="/cadastro">Primeira vez? Cadastre-se!</StyledLink>
         </Container>
     )
 }
-
-const Container = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    height: 100vh;
-    box-sizing: border-box;
-    padding: 25px;
-    background: #8C11BE;
-`
-
-const Title = styled.h1`
-    margin-bottom: 24px;
-    font-family: "Saira Stencil One";
-    font-weight: 400;
-    font-size: 32px;
-    line-height: 50px;
-    text-align: center;
-    color: #FFFFFF;
-`
-
-const Form = styled.form`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-`
-
-const StyledLink = styled(Link)`
-  margin-top: 36px;
-  font-size: 14px;
-  font-weight: 700;
-  font-size: 15px;
-  line-height: 18px; ;
-  text-align: center;
-  text-decoration: none;
-  color: #FFFFFF;
-`;
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,8 +1,23 @@
-import styled from "styled-components";
-import axios from "axios";
+import React from "react";
+//import axios from "axios";
+// import { UserContext } from "../context/UserContext"
+import { Input } from "../components/Input"
+import { Button } from "../components/Button"
+import { Container, Title, Form, StyledLink } from "../style/commonStyles"
+
 
 export default function SignUp() {
     return (
-        <></>
+        <Container>
+            <Title>MyWallet</Title>
+            <Form>
+                <Input name="name" type="text" placeholder="Nome" />
+                <Input name="email" type="email" placeholder="E-mail" />
+                <Input name="password" type="password" placeholder="Senha" />
+                <Input name="password" type="password" placeholder="Confirme a senha" />
+                <Button type="submit" >Cadastrar</Button>
+            </Form>
+            <StyledLink to="/">Já tem uma conta? Entre agora!</StyledLink>
+        </Container>
     )
 }
