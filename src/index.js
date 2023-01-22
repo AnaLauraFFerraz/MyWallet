b0a43c95f7424b707e43614d6d3f5ff5b18bb953
AnaLauraFFerraz <analaurafferraz@gmail.com>
2023-01-22 17:17:35 -0300

    global style

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GlobalStyle from "./style/GlobalStyle"
-import App from './App'
+import { GlobalStyle } from "./style/GlobalStyle.js";
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
